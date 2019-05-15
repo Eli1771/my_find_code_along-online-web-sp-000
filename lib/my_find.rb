fa933ccfd55361f17b60059c2082adfaaf5959b1
elijahmm17 <elijahmm17@gmail.com>
2019-05-15 19:39:55 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = o 
+  results = []
+  while i < collection.length do 
+    if yield(collection[i]) == true 
+      results << collection[i]
+    end
+    i += 1 
+  end 
+  results
 end
